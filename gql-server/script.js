070c012df641c79c84c273fe6b5090e0076c1633
Adegoke Temitope <adegoketemitope1909@gmail.com>
2022-07-09 16:19:53 +0100

    use string

--- gql-server/script.js
+++ gql-server/script.js
@@ -4,40 +4,40 @@ const jsonTransactions = require("./graphql-server.json");
 
 const account = {
   "Marshall Tucker": {
-    accountNumber: 4088978422,
+    accountNumber: "4088978422",
     email: "marvajustice@isosure.com",
     gender: "Female",
-    phone: 09123945623,
+    phone: "09123945623",
   },
   "Baker Espinoza": {
-    accountNumber: 7165405534,
+    accountNumber: "7165405534",
     email: "serenadillon@zounds.com",
     gender: "Male",
-    phone: 08034921121,
+    phone: "08034921121",
   },
   "Cynthia Harrell": {
-    accountNumber: 5072026701,
+    accountNumber: "5072026701",
     email: "angelitajohns@speedbolt.com",
     gender: "Female",
-    phone: 091880234212,
+    phone: "091880234212",
   },
   "Tyson Wells": {
-    accountNumber: 8232293491,
+    accountNumber: "8232293491",
     email: "gallagheroconnor@geekwagon.co",
     gender: "Male",
-    phone: 09199331423,
+    phone: "09199331423",
   },
   "Evangeline Ortega": {
-    accountNumber: 2303696901,
+    accountNumber: "2303696901",
     email: "glendalawson@zoxy.com",
     gender: "Female",
-    phone: 09199332423,
+    phone: "09199332423",
   },
   "Desiree Watkins": {
-    accountNumber: 3317162382,
+    accountNumber: "3317162382",
     email: "morenoestes@centrexin.com",
     gender: "Male",
-    phone: 08065349232,
+    phone: "08065349232",
   },
 };
 
